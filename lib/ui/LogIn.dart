3a6e85a9084135c64a62c6738e622cef790c4699
mac <mac@2-2.local>
2026-07-08 10:32:20 +0800

    sophia: change LogIn page

--- lib/ui/LogIn.dart
+++ lib/ui/LogIn.dart
@@ -7,9 +7,34 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF151E2A),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(
+          ShaderMask(
+            shaderCallback: (Rect rect) {
+              return LinearGradient(
+                colors: <Color>[Color(0xFF0CA8DB), Color(0xFF4C76DB)],
+              ).createShader(rect);
+            },
+            child: Text(
+              "Welcome back!",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 30,
+              ),
+            ),
+          ),
+
+          Container(width: 350, height: 50, child: TextField()),
+
+          Container(width: 350, height: 50, child: TextField()),
+
+          Container(
+            width: 400,
+            height: 50,
             child: OutlinedButton(
               onPressed: () {
                 Navigator.push(
